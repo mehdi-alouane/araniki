7db337661c6eb1537c313889c935467dc7e8b660
=mehdi-alouane <=pro4medo@gmail.com>
2016-01-11 12:16:11 +0000

    update

--- backend/app/user/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require
-$config_file_path = '../../lib/hybridauth/config.php';
-$link = new Hybrid_Auth($config_file_path);
- $adapter = $link->authenticate( "Twitter" );
- $user_profile = $adapter->getUserProfile();
- 
- echo "user: " . $user_profile->displayName;
-?>
--- front-end/app/scripts/app.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $('.signup_form').addClass('animated shake');	
-});
