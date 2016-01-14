4431e1d83e31afcb184b7c1af5be88f30e7512ed
=mehdi-alouane <=pro4medo@gmail.com>
2016-01-14 21:33:38 +0000

    update gulpfile & scss

--- frontend/app/gulpfile.js
+++ frontend/app/gulpfile.js
@@ -1,36 +1,39 @@
 var gulp = require('gulp');
 var	sass = require('gulp-sass');
-var	server = require('gulp-server-livereload');
-var	minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
 
+    browserSync.init({
+        server: "./"
+    });
+    notify : false
 
-//sass task
+    gulp.watch("styles/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    gulp.src('styles/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'))
+    return gulp.src("styles/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/styles/"))
+        .pipe(browserSync.stream());
 });
 
-//Watch task
-gulp.task('sass',function() {
-    gulp.watch('scss/**/*.scss',['sass']);
-});
+gulp.task('default', ['serve']);
 
-//server task
-gulp.task('webserver', function(){
-	gulp.src('../app/')
-	.pipe(server({
-		defaultFile: 'index.html',
-		livereload: true,
-		directoryListing : true,
-		open: true,
-		}));
-	});
+/*
+//prefix task
 
- // mignify css task
-gulp.task('mincss', function() {
-  return gulp.src('../dist/*.css')
-    .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('min-css'));
+gulp.task('default', function () {
+    return gulp.src('dist/styles/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/styles/prefixcss'));
 });
+*/
